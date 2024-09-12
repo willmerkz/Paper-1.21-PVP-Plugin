7d9dfa5595cf641caf3a49cf208852e68e6efd5d
willmerkz <willmerkle2k@gmail.com>
2024-09-11 21:38:38 -0400

    first commit

---
+++ src/main/java/io/github/willmerkz/team2PVP/Team2PVP.java
@@ -0,0 +1,17 @@
+package io.github.willmerkz.team2PVP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Team2PVP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
